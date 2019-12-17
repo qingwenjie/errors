c03357f00395f6c0eb95155f1bca80d62988562f
qwj <wjqing@landhightech.com>
2019-12-17 17:47:02 +0800

    初始化

--- demo/test.go
+++ demo/test.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	errors2 "errors"
+	"fmt"
 	"github.com/qingwenjie/errors"
 )
 
 func main() {
-	err := errors2.New("test error")
-	errors.Trace(err)
+	err := err1()
+	e := errors.TraceWithField(err, "f1", "aaaa")
+	fmt.Println(e)
+}
+
+func err1() error {
+	c := testErr{}
+	return c.error1()
+}
+
+type testErr struct{}
+
+func (s *testErr) error1() error {
+	err := s.error2()
+	return errors.Trace(err)
+}
+
+func (s *testErr) error2() error {
+	err := errors2.New("test error2")
+	return errors.Trace(err)
 }
